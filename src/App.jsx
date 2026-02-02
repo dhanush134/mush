25427fef24efe980bbafd1de8c485fd19c2bce1b
rake3897 <131149368+rake3897@users.noreply.github.com>
2026-02-02 11:38:50 +0530

    changes deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import BatchList from './components/BatchList';
 import BatchForm from './components/BatchForm';
 import BatchDetail from './components/BatchDetail';
@@ -8,7 +8,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="app">
         <Routes>
           <Route path="/" element={<BatchList />} />
@@ -18,6 +18,6 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
